aababb8f2d582314a55b8f966bd1ce8a3fc5cc19
NxxhL <89213023+chifouu65@users.noreply.github.com>
2022-12-12 19:58:13 +0100

    .patch key error for product.

--- kasa/src/pages/Product/Product.jsx
+++ kasa/src/pages/Product/Product.jsx
@@ -55,7 +55,7 @@ function Product() {
                                     <div>
                                         {
                                             product.equipments.map((item) => (
-                                                <li key={""}>{item}</li>
+                                                <li key={item.length}>{item}</li>
                                             ))
                                         }
                                     </div>
